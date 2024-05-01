9e79b63c5b63f85a5367af6cf7ab9d6d9fea12d3
ALBERTO_CARBAJAL_VAZQUEZ <a.21308051280449@cbtis128.edu.mx>
2024-05-01 22:00:52 +0000

    update

---
+++ lib/spacearound.dart
@@ -0,0 +1,42 @@
+import 'package:act3carbajal/drawer_menu.dart';
+import 'package:flutter/material.dart';
+
+class Contact extends StatelessWidget {
+  static const String routeName = '/contact';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("spaceAround carbajal"),
+          backgroundColor: Color(0xff00ffc0)),
+      drawer: DrawerMenu(),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(0.0),
+            color: Colors.cyanAccent,
+            width: 80.0,
+            height: 80.0,
+          ),
+          Container(
+            padding: const EdgeInsets.all(0.0),
+            color: Colors.blueAccent,
+            width: 80.0,
+            height: 80.0,
+          ),
+          Container(
+            padding: const EdgeInsets.all(0.0),
+            color: Colors.orangeAccent,
+            width: 80.0,
+            height: 80.0,
+          ),
+        ],
+      ),
+// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
